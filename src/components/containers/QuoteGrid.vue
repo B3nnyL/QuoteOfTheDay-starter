e6b302e46eefd140a5798586af5d562c865c1a41
Shuying Lin <shuyinglin47@gmail.com>
2018-07-06 22:18:04 +1000

    add quote array

--- src/components/containers/QuoteGrid.vue
+++ src/components/containers/QuoteGrid.vue
@@ -14,13 +14,28 @@
     name: 'quoteGrid',
     data: function () {
       return {
-        quotes: [],
+        mocks: [{
+          id: 123,
+          content: 'And if you gaze long into an abyss, the abyess will also gae into thee. ',
+          hearts: 233,
+          author: 'friderich nietsche',
+          contributor: '@wlfp',
+          socialUrl: 'https://www.twitter.com/_wlfp'
+        },
+        {
+          id: 456,
+          content: 'Knowledge is the most democratic source of power. ',
+          hearts: 777,
+          author: 'Alvin Toffler',
+          contributor: '@wlfp',
+          socialUrl: 'https://www.twitter.com/_wlfp'
+        }],
         quote: {
           id: 123,
           content: 'And if you gaze long into an abyss, the abyess will also gae into thee. ',
           hearts: 233,
           author: 'friderich nietsche',
-          contributor: '@wlfp from Devhub',
+          contributor: '@wlfp',
           socialUrl: 'https://www.twitter.com/_wlfp'
         }
       }
